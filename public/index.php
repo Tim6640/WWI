691b56b3ff6b8352b04676fa9ee41d5114aef8f8
Tim van Beek <tim1195.vanbeek@gmail.com>
2019-11-12 23:18:32 +0100

    Setup

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim11
+ * Date: 7-11-2019
+ * Time: 09:52
+ */
+include_once("init.php");
+include_once("includes/header.php");
+?>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="app"></div>
+    <!--
+      This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the <body> tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+    -->
+    </body>
+    </html>
+<? include_once("public/includes/footer.php");
+?>
