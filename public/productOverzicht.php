06aa5aacb3e4e78855d45564e6134c3e7be26362
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-11-13 21:02:29 +0100

    added productOverzicht file basis

---
+++ public/productOverzicht.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <style>
+
+        .card {
+            background-color: lightblue;
+            border: solid;
+            border-radius: 25px;
+            display: flex;
+        }
+        .card-img-top {
+        }
+        .card-body {
+        }
+        .card-title {
+        }
+        .card-text {
+        }
+        .btn-cart{
+        }
+        .btn-wishlist{
+        }
+
+    </style>
+
+</head>
+<body>
+<header>
+    <script src="https://kit.fontawesome.com/6756a99abc.js" crossorigin="anonymous"></script>
+    <div class=".header"></div>
+</header>
+
+
+<div class="card" ;">
+    <img src="" class="card-img-top" alt="">
+    <div class="card-body">
+        <h5 class="card-title">Titel | Rating | Prijs</h5>
+        <p class="card-text">Hier komt de product beschrijving zoals wat het kan, welke kleur het heeft of het kan vliegen etc</p>
+        <a href="#" class="btn-cart">winkelwagen</a>
+        <a href="#" class="btn-wishlist">wishlist</a>
+    </div>
+</div>
+
+
+
+
+<!--put html footer here-->
+
+</body>
+</html>
