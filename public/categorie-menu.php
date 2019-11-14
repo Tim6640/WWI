f3795bf93673127eb758d4fa3b68a80f145a148a
maxbaart <baartmanmax@gmail.com>
2019-11-14 12:22:11 +0100

    Create categorie-menu.php
    do something

---
+++ public/categorie-menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head lang="eng">
+    <title>Paginanaam</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/11b906cc61.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="categorie-menu-layout.css">
+</head>
+
+<body>
+
+<header class="bgimage">
+    <div class="container">
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+        </div>
+    </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
