1ed9fb4efd3798fe19542e7f398301c8573350c0
maxbaart <baartmanmax@gmail.com>
2019-11-14 21:10:39 +0100

    Update categorie-menu.php

--- public/categorie-menu.php
+++ public/categorie-menu.php
@@ -7,21 +7,77 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/11b906cc61.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="categorie-menu-layout.css">
+
 </head>
-
 <body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tim11
+ * Date: 7-11-2019
+ * Time: 09:52
+ */
+include_once("../src/core/init.php");
+$pageTitle = "Home";
+include_once("../public/includes/header.php");
 
-<header class="bgimage">
-    <div class="container">
-    </div>
-</header>
-<div class="container">
+//include_once(PUBLIC_ROOT."includes/footer.php");
+?>
+
+
+<h3>Productcategorieën:</h3>
+<div class="container text-center">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
         </div>
     </div>
 </div>
 
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+    </div>
+</div>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+        <div class="col">
+            <i class="fas fa-gamepad fa-4x"></i>
+            <h5>Categorie 1</h5>
+        </div>
+    </div>
+</div>
+
+<!-- Dit is zijn de body-includes -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
