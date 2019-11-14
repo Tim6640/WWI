140115e04513cdb9888d44ff15cf366f8dd72582
koenradar <s1147498@student.windesheim.nl>
2019-11-14 12:41:39 +0100

    Included header, more test added

--- public/includes/product.php
+++ public/includes/product.php
@@ -1,4 +1,8 @@
 <!doctype html>
+<?php
+include_once "header.php";
+$pageTitle = "product"
+?>
 <html lang="Ne">
 <head>
     <!--    # benodigde meta tags-->
@@ -23,7 +27,7 @@
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
-    <div class="carousel-inner">
+    <div class="carousel-inner" >
         <div class="carousel-item active">
             <img class="d-block w-100" src="test1.jpg" alt="First slide">
         </div>
@@ -31,7 +35,7 @@
             <img class="d-block w-100" src="test2.jpg" alt="Second slide">
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="test1.jpg" alt="Third slide">
+            <img class="d-block w-100" src="test3.jpg" alt="Third slide">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -42,7 +46,9 @@
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
+<div class="">
 
+</div>
 </div>
 
 
