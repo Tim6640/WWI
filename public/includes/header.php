6050e2e92cc0cf8ae3ddacbb4581aa7ca9d5dc7b
Tim van Beek <tim.van.beek@ivention.nl>
2019-11-15 14:00:56 +0100

    Header

--- public/includes/header.php
+++ public/includes/header.php
@@ -20,28 +20,27 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </head>
 <body>
-
-<nav class="navbar bg-light">
-    <div class="container-fluid p-0">
+<div class="bg-primary">
+    <div class="container p-3">
         <div class="row">
-            <a class="col-2 pr-0" href="<?=__ROOT__?>/public/index.php">
-                <img class="logo-small img-fluid" src="images/wwi_logo_small.png" alt="">
+            <a class="col-2 col-md-1 pr-0" href="<?=__ROOT__?>/public/index.php">
+                <img class="logo-small m-auto" src="images/wwi_logo_small.png" alt="">
             </a>
-            <a class="col-6 my-auto" href="<?=__ROOT__?>/index.php">
+            <a class="col-6 col-md-7 my-auto pl-md-0" href="<?=__ROOT__?>/index.php">
                 <h1 class="title">Wide World <span>Importers</span></h1>
             </a>
-            <span class="col-4 my-auto icons text-center">
-                <a class="mr-3">
+            <span class="col-4 my-auto icons text-right">
+                <a class="mr-3 mr-md-5">
                     <i class="fas fa-user-alt"></i>
                 </a>
-                <a>
+                <a class="mr-md-3">
                     <i class="fas fa-shopping-cart"></i>
                 </a>
             </span>
-            <a class="col-2 pr-0" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="col-2 col-md-1 pr-0 text-center" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="menu-button my-auto"><i class="fas fa-bars black"></i></span>
             </a>
-            <form class="col-10 pl-0 input-group" method="post">
+            <form class="col-10 col-md-11 pl-0 input-group" method="post">
                 <input class="search form-control mr-0" type="text" name="search" placeholder="Zoeken">
                 <div class="input-group-append">
                     <span class="input-group-text icons"><i class="fas fa-filter"></i></span>
@@ -49,4 +48,4 @@
             </form>
         </div>
     </div>
-</nav>
+</div>
