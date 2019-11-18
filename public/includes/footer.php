e1a55628dbf8c0c8ea5d55c2f8a78e24cf0c8d06
stanvdveen <stanvanderveen@yahoo.com>
2019-11-18 16:16:09 +0100

    footer v1

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -7,10 +7,55 @@
  */
 
 ?>
-<div class="footer">
-    <img class="logo-small" alt="logo" src="public/images/wwi_logo.png">
-    <p>copyright ICTM1e2</p>
-</div>
+dafsf
+<!-- Footer -->
+<footer class="bg-primary page-footer">
+    <div class="container pt-3">
+        <div class="row">
+            <div class="col-4">
+                <img class="logo-small" src="images/wwi_logo_small.png">
+                <h1 class="title">Wide World <span>Importers</span></h1>
+            </div>
+            <div class="col-4">
+                <h6 class="text-uppercase">over ons</h6>
+                <ul class="list-unstyled">
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="fas fa-at"> E-mail</i></a>
+                    </li>
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="fas fa-phone-alt"> Telefoonnummer</i></a>
+                    </li>
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="fas fa-map-marker"> Adres</i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-4">
+                <h6 class="text-uppercase">Overig</h6>
+                <ul class="list-unstyled">
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="far fa-newspaper"> Nieuwsbrief</i></a>
+                    </li>
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="fab fa-facebook"> Facebook</i></a>
+                    </li>
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="fab fa-twitter-square"> Twitter</i></a>
+                    </li>
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="fab fa-instagram"> Instagram</i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
+    <!-- Copyright -->
+    <div class="footer-copyright bg-secondary text-center">© ICT1ME2 Windesheim
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 </body>
 </html>
