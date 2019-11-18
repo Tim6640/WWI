7524428973d34656188505ba566b33fb1580e396
koenradar <s1147498@student.windesheim.nl>
2019-11-18 16:08:18 +0100

    Inclusion of sql and updated some buttons to work

--- public/product.php
+++ public/product.php
@@ -21,29 +21,64 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
             crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/3c0f9f28b6.js" crossorigin="anonymous"></script>
     <title>Product pagina</title>
 </head>
+
+<?php
+
+// Logica
+if (isset($_GET["pid"])) {
+    $productNummer = $_GET["pid"];
+
+
+} else {
+    print ("helaas bestaat het gezochte product niet<br>");
+    print ("klik <a href='/wwi/public'>hier</a> om terug te gaan naar de thuispagina");
+}
+
+// SQL SETUP
+
+$host = "localhost";
+$databasename = "wideworldimporters";
+$user = "root";
+$pass = null;
+$port = 3306;
+$connection = mysqli_connect($host, $user, $pass, $databasename, $port);
+$sql = "SELECT StockItemName, RecommendedRetailPrice FROM stockitems WHERE StockItemID=?";
+$statement = mysqli_prepare($connection, $sql);
+mysqli_stmt_bind_param($statement, 'i', $productNummer);
+mysqli_stmt_execute($statement);
+$result = mysqli_stmt_get_result($statement);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$name = $row["StockItemName"];
+$price = $row["RecommendedRetailPrice"];
+
+
+?>
 <!--# einde voorwerk-->
 <!--# begin pagina-->
 <body>
 <div class="container">
-    <h1> Product naam</h1>
     <div class="row">
-        <button type="button" class="btn btn-default btn-sm">
-            <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
-        </button>
-        <button type="button" class="btn btn-default btn-sm">
-            <span class="glyphicon glyphicon-star"></span> Wishlist
-        </button>
-
+        <div class="col-9">
+            <h2> <?php print($name); ?></h2>
+        </div>
+        <div class="col-3">
+            <a href="betalingbevestiging.php"> <i class="fas fa-cart-plus" style="font-size:10vw;"></i> </a>
+            <a href="betalingbevestiging.php"> <i class="fas fa-heart" style="font-size:10vw;"></i> </a>
+        </div>
     </div>
+<!--    Carrousel start-->
     <div class="col-12">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div id="carousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselIndicators" data-slide-to="3"></li>
             </ol>
+<!--            Carrousel inner items-->
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img class="d-block w-100" src="test1.jpg" alt="First slide">
@@ -54,36 +89,46 @@
                 <div class="carousel-item">
                     <img class="d-block w-100" src="test3.jpg" alt="Third slide">
                 </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="test3.jpg" alt="Third slide">
+                </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+<!--            Carrousel controls-->
+            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
+<!--            Carrousel small indicators help me with the scaling-->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active">
+                    <img class="d-block w-100" src="test1.jpg">
+                </li>
+                <li data-target="#carousel" data-slide-to="1">
+                    <img class="d-block w-100" src="test2.jpg">
+                </li>
+                <li data-target="#carousel" data-slide-to="2">
+                    <img class="d-block w-100" src="test3.jpg">
+                </li>
+                <li data-target="#carousel" data-slide-to="2">
+                    <img class="d-block w-100" src="test3.jpg">
+                </li>
+            </ol>
         </div>
     </div>
-    <div class="row">
-        <div class="col-3">
-            <img class="img-fluid" src="test1.jpg" alt="">
-        </div>
-        <div class="col-3">
-            <img class="img-fluid" src="test2.jpg" alt="">
-        </div>
-        <div class="col-3">
-            <img class="img-fluid" src="test3.jpg" alt="">
-        </div>
-    </div>
+<!--    items under the carrousel-->
     <div class="row">
         <div class="col-4">
-            <h2>Prijs</h2>
+            <h2><?php print("€" . $price); ?></h2>
         </div>
         <div class="col-6">
             <h2>Bezorginformatie</h2>
         </div>
     </div>
+<!--    style for css needs to be moved-->
     <style type="text/css">
         .h-divider {
             margin-top: 5px;
@@ -95,29 +140,35 @@
     </style>
     <div class="h-divider">
     </div>
+<!--    needs intermediate database  -->
     <div class="col-12">
         <h4>Productbeschrijving</h4>
     </div>
     <div class="col-12">
         <div class="panel-body" align="centre">
-            Uitgebreide beschrijving van een product die over meerdere paginas kan gaan over een product dit is gewoon filler
+            Uitgebreide beschrijving van een product die over meerdere paginas kan gaan over een product dit is gewoon
+            filler
         </div>
     </div>
     <div class="col-12" align="middle">
         <div class="text-centre">
-        <button type="button" class="btn btn-primary btn-lg bnt-block">
-            <span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart
-        </button>
+            <button type="button" class="btn btn-primary btn-lg bnt-block">
+                <a class="fas fa-cart-plus" href="winkelmandje.php"> Shopping Cart </a>
+            </button>
         </div>
         <div class="h-divider">
         </div>
         <div>
             <h1>review space</h1>
         </div>
+    </div>
 </div>
-</div>
-
-
+<?PHP
+//close connection
+mysqli_stmt_close($statement);
+mysqli_free_result($result);
+mysqli_close($connection);
+?>
 </body>
 
 
