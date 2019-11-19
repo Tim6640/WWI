109274fb8fe89286dd41599886314e6e82e6cf98
stanvdveen <stanvanderveen@yahoo.com>
2019-11-19 12:29:25 +0100

    footer is af

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -7,15 +7,27 @@
  */
 
 ?>
-dafsf
+<!-- webpagina code-->
+...
+<br>
+...
+<br>
+...
+<br>
+...
+<!-- webpagina code-->
 <!-- Footer -->
+
 <footer class="bg-primary page-footer">
     <div class="container pt-3">
         <div class="row">
+            <!-- logo -->
             <div class="col-4">
-                <img class="logo-small" src="images/wwi_logo_small.png">
-                <h1 class="title">Wide World <span>Importers</span></h1>
+                <img class="footer-logo " src="images/wwi_logo_small.png">
+                <h6 class="footer-title">Wide World <span class="text-secondary">Importers</span></h6>
             </div>
+            <!-- logo -->
+            <!-- Over ons -->
             <div class="col-4">
                 <h6 class="text-uppercase">over ons</h6>
                 <ul class="list-unstyled">
@@ -23,19 +35,21 @@ dafsf
                         <a class="text-secondary" href="#!"><i class="fas fa-at"> E-mail</i></a>
                     </li>
                     <li>
-                        <a class="text-secondary" href="#!"><i class="fas fa-phone-alt"> Telefoonnummer</i></a>
+                        <a class="text-secondary" href="#!"><i class="fas fa-phone-alt"> Telefoon</i></a>
                     </li>
                     <li>
                         <a class="text-secondary" href="#!"><i class="fas fa-map-marker"> Adres</i></a>
                     </li>
-                </ul>
-            </div>
-            <div class="col-4">
-                <h6 class="text-uppercase">Overig</h6>
-                <ul class="list-unstyled">
                     <li>
                         <a class="text-secondary" href="#!"><i class="far fa-newspaper"> Nieuwsbrief</i></a>
                     </li>
+                </ul>
+            </div>
+            <!-- Over ons -->
+            <!-- Overig -->
+            <div class="col-4">
+                <h6 class="text-uppercase">Overig</h6>
+                <ul class="list-unstyled">
                     <li>
                         <a class="text-secondary" href="#!"><i class="fab fa-facebook"> Facebook</i></a>
                     </li>
@@ -45,8 +59,12 @@ dafsf
                     <li>
                         <a class="text-secondary" href="#!"><i class="fab fa-instagram"> Instagram</i></a>
                     </li>
+                    <li>
+                        <a class="text-secondary" href="#!"><i class="fas fa-question-circle"></i> FAQ</i></a>
+                    </li>
                 </ul>
             </div>
+            <!-- Overig -->
         </div>
     </div>
 
