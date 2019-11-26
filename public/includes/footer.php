d6d4bcc19522adab010fb6d5fdcc9f5fc48406ef
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-11-26 17:27:21 +0100

    basis voor e-mail in footer toegevoegd

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -18,45 +18,35 @@
                 <img class="footer-logo " src="images/wwi_logo_small.png">
                 <h6 class="footer-title">Wide World <span class="text-secondary">Importers</span></h6>
             </div>
-            <!-- logo -->
             <!-- Over ons -->
             <div class="col-4">
                 <h6 class="text-uppercase text-white">over ons</h6>
                 <ul class="list-unstyled">
-                    <li>
-                        <a class="text-white" href="#!"><i class="fas fa-at"> E-mail</i></a>
-                    </li>
-                    <li>
-                        <a class="text-white" href="#!"><i class="fas fa-phone-alt"> Telefoon</i></a>
-                    </li>
-                    <li>
-                        <a class="text-white" href="#!"><i class="fas fa-map-marker"> Adres</i></a>
-                    </li>
-                    <li>
-                        <a class="text-white" href="#!"><i class="far fa-newspaper"> Nieuwsbrief</i></a>
-                    </li>
+                    <li><a class="text-white" href="#!"><i class="fas fa-at"> E-mail</i></a></li>
+                    <li><a class="text-white" href="#!"><i class="fas fa-phone-alt"> Telefoon</i></a></li>
+                    <li><a class="text-white" href="#!"><i class="fas fa-map-marker"> Adres</i></a></li>
+                    <li><a class="text-white" href="#!"><i class="far fa-newspaper"> Nieuwsbrief</i></a></li>
                 </ul>
             </div>
-            <!-- Over ons -->
             <!-- Overig -->
             <div class="col-4">
                 <h6 class="text-uppercase text-white">Overig</h6>
                 <ul class="list-unstyled">
-                    <li>
-                        <a class="text-white" href="#!"><i class="fab fa-facebook"> Facebook</i></a>
-                    </li>
-                    <li>
-                        <a class="text-white" href="#!"><i class="fab fa-twitter-square"> Twitter</i></a>
-                    </li>
-                    <li>
-                        <a class="text-white" href="#!"><i class="fab fa-instagram"> Instagram</i></a>
-                    </li>
-                    <li>
-                        <a class="text-white text-decoration-none" href="#!"><i class="fas fa-question-circle"></i> FAQ</i></a>
-                    </li>
+                    <li><a class="text-white" href="#!"><i class="fab fa-facebook"> Facebook</i></a></li>
+                    <li><a class="text-white" href="#!"><i class="fab fa-twitter-square"> Twitter</i></a></li>
+                    <li><a class="text-white" href="#!"><i class="fab fa-instagram"> Instagram</i></a></li>
+                    <li><a class="text-white text-decoration-none" href="#!"><i class="fas fa-question-circle"></i> FAQ</i></a></li>
                 </ul>
             </div>
-            <!-- Overig -->
+        </div>
+        <div class="row">
+            <h5 class="text-white">Inschrijven Nieuwsbrief&nbsp</h5><br>
+            <form method="post">
+                <input type="email" name="email" value="John.Doe@voorbeeld.nl"><br>
+                Ik geef toestemming dat WWI mij e-mails stuurt
+                <input type="checkbox" name="permission">
+                <input type="submit" value="verzenden">
+            </form>
         </div>
     </div>
 
