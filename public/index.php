ef3a749e6f8cbe1ff69f7d6dd68f0d8e0516d85d
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-11-29 12:21:48 +0100

    moved newsletter from footer to index

--- public/index.php
+++ public/index.php
@@ -9,7 +9,38 @@
 include_once("../src/core/init.php");
 $pageTitle = "Home";
 include_once("../public/includes/header.php");
-include_once("../public/includes/footer.php");
 
+?>
+        <div class="row">
+            <h5 class="text-white">Inschrijven Nieuwsbrief&nbsp</h5>
+            <form method="post">
+                <input type="email" name="email"><br>
+                <p style="color:white">Ik geef toestemming dat WWI mij e-mails verstuurt
+                <input type="checkbox" id="termsChkbx " onclick="change_button(this,'sub1')"></p>
+                <p><input type="submit" name="submit" value="verzenden" id="sub1" disabled="disabled"></p>
+            </form>
+            <script type = "text/javascript">
+                function change_button(checkbx,button_id) {
+                    var btn = document.getElementById(button_id);
+                    if (checkbx.checked == true) {
+                        btn.disabled = "";
+                    } else {
+                        btn.disabled = "disabled";
+                    }
+                }
+            </script>
+        </div>
+    </div>
+
+<?php
+    if (isset($_POST["verzenden"])){
+        $db = new DbHandler();
+        $connection = $db->connect();
+        $sql = "INSERT INTO subscription VALUES (email=?)";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute([':id' => $_POST["email"]]);
+        $db->disconnect();
+        $db = null;
+    }
 
 include_once("../public/includes/footer.php");
