ee04016b3cee052cac08c74f9031406eed463bd6
Tim van Beek <tim1195.vanbeek@gmail.com>
2019-12-01 18:52:38 +0100

    account menu

---
+++ public/account.php
@@ -0,0 +1,24 @@
+<?php
+$pageTitle = "Account";
+include_once("includes/header.php");
+?>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/account.css">
+</head>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3 col-12 side-bar p-2">
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action p-1"><i class="fas fa-user-cog mr-2"></i>Mijn gegevens</a>
+                <a href="#" class="list-group-item list-group-item-action p-1"><i class="fas fa-history mr-2"></i>Bestel geschiedenis</a>
+                <a href="#" class="list-group-item list-group-item-action p-1"><i class="fas fa-id-card-alt mr-2"></i>Registreren membership</a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php
+include_once("includes/footer.php");
+?>
+
