72032f81532053763a4df21af76e258290d7bb18
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-12-03 21:53:25 +0100

    moved mail field from footer to index & add new db

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -39,44 +39,11 @@
                 </ul>
             </div>
         </div>
-        <div class="row">
-            <h5 class="text-white">Inschrijven Nieuwsbrief&nbsp</h5>
-            <form method="post">
-                <input type="email" name="email"><br>
-                <p style="color:white">Ik geef toestemming dat WWI mij e-mails verstuurt
-                <input type="checkbox" id="termsChkbx " onclick="change_button(this,'sub1')"></p>
-                <p><input type="submit" name="submit" value="verzenden" id="sub1" disabled="disabled"></p>
-            </form>
-            <script type = "text/javascript">
-                function change_button(checkbx,button_id) {
-                    var btn = document.getElementById(button_id);
-                    if (checkbx.checked == true) {
-                        btn.disabled = "";
-                    } else {
-                        btn.disabled = "disabled";
-                    }
-                }
-            </script>
-        </div>
     </div>
-
-<?php
-    if (isset($_POST["verzenden"])){
-        $db = new DbHandler();
-        $connection = $db->connect();
-        $sql = "INSERT INTO subscription VALUES (email=?)";
-        $stmt = $connection->prepare($sql);
-        $stmt->execute([':id' => $_POST["email"]]);
-        $db->disconnect();
-        $db = null;
-    }
-    ?>
-
     <!-- Copyright -->
     <div class="footer-copyright bg-secondary text-center">© ICTM1e2 Windesheim
     </div>
     <!-- Copyright -->
-
 </footer>
 <!-- Footer -->
 </body>
--- public/index.php
+++ public/index.php
@@ -6,17 +6,28 @@
  * Time: 09:52
  */
 
-include_once("../src/core/init.php");
+include_once("../src/core/DbHandler.php");
 $pageTitle = "Home";
 include_once("../public/includes/header.php");
 
+if (isset($_POST["email"])) {
+    $db = new DbHandler("USER");
+    $connection = $db->connect();
+    $sql = "INSERT INTO newsletter (email) VALUES (:id)";
+    $stmt = $connection->prepare($sql);
+    $stmt->execute([':id' => $_POST["email"]]);
+    $db->disconnect();
+    $db = null;
+}
+
 ?>
+    <div class="container pt-3">
         <div class="row">
-            <h5 class="text-white">Inschrijven Nieuwsbrief&nbsp</h5>
-            <form method="post">
-                <input type="email" name="email"><br>
-                <p style="color:white">Ik geef toestemming dat WWI mij e-mails verstuurt
-                <input type="checkbox" id="termsChkbx " onclick="change_button(this,'sub1')"></p>
+            <h5>Inschrijven Nieuwsbrief&nbsp</h5>
+            <form method="post" action="">
+                <input type="email" name="email" placeholder="bla@bla.nl">
+                <p>Ik geef toestemming dat WWI mij e-mails verstuurt
+                    <input type="checkbox" id="termsChkbx " onclick="change_button(this,'sub1')"></p>
                 <p><input type="submit" name="submit" value="verzenden" id="sub1" disabled="disabled"></p>
             </form>
             <script type = "text/javascript">
@@ -33,14 +44,4 @@ include_once("../public/includes/header.php");
     </div>
 
 <?php
-    if (isset($_POST["verzenden"])){
-        $db = new DbHandler();
-        $connection = $db->connect();
-        $sql = "INSERT INTO subscription VALUES (email=?)";
-        $stmt = $connection->prepare($sql);
-        $stmt->execute([':id' => $_POST["email"]]);
-        $db->disconnect();
-        $db = null;
-    }
-
 include_once("../public/includes/footer.php");
