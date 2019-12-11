2dde61838986ec220932a41ff2ae1654187271a6
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-12-11 22:53:45 +0100

    code displays orderhistory correctly without style

--- public/orderGeschiedenis.php
+++ public/orderGeschiedenis.php
@@ -14,9 +14,9 @@ if (isset($userID)) {
     print "Hier komt de order geschiedenis van de gebruiker<br>";
 
     //start db connectie
-    $db = new DbHandler("ERP");
+    $db = new DbHandler("USER");
     $connection = $db->connect();
-    $sql = "SELECT O.orderrecordID, OP.orderproductID, O.date, OP.productID, OP.amount FROM orderrecord O JOIN orderproduct OP ON O.orderrecordID=OP.orderrecordID WHERE O.customerID=:id";
+    $sql = "SELECT O.orderrecordID, O.date, OP.productID, OP.amount FROM orderrecord O JOIN orderproduct OP ON O.orderrecordID=OP.orderrecordID WHERE O.customerID=:id";
     $stmt = $connection->prepare($sql);
     $stmt->execute([':id' => $userID]);
     $results = $stmt->fetchAll();
@@ -24,17 +24,26 @@ if (isset($userID)) {
     $db->disconnect();
     $db = null;
 
+
+    $inOrder=0;
     foreach($results as $result) {
-        print "test";
-        print ($result["date"]);
+        $recordID=($result["orderrecordID"]);
+        $date = ($result["date"]);
+        $pid= ($result["productID"]);
+        $amount= ($result["amount"]);
+
         //if orderrecordID is the same as previous orderrecord make new div
-
-        //else add to div
-
-        //print orderrecordID
-        //print date
+        if ($recordID!=$inOrder) {
+//            dit komt in een nieuwe div
+            print "<br> Ordernummer: $recordID";
+            print "&nbsp&nbsp&nbsp op $date<br>";
+        }
 
         //insert items into orderrecordID div
+        print "Product: ".$pid." ".$amount." keer<br>";
+
+        //make clear for next loop that previous orderrecord==$inOrder
+        $inOrder=$recordID;
     }
 } else {
     //wanneer er geen geschiedenis is van de bestelling
