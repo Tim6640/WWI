942e66660c2e0370c3e717b7e5df0ee6250bd1e9
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-12-13 13:20:24 +0100

    added func on betalingbevestiging to process order

--- public/betalingbevestiging.php
+++ public/betalingbevestiging.php
@@ -14,7 +14,7 @@
      * Date: 7-11-2019
      * Time: 09:52
      */
-    include_once("../src/core/init.php");
+    include_once("../src/core/DbHandler.php");
     $pageTitle = "Home";
     include_once("../public/includes/header.php");
     ?>
@@ -22,9 +22,79 @@
 <body>
 <div class="container my-5 text-center" role="alert" style="">
     <?php
+    //check if bestiging is set else 404 page
     if(isset($_GET['bevestiging'])) {
+        //check if bestiging is set to "proceed" else set error
         $bevestiging = $_GET['bevestiging'];
-        if ($bevestiging == "proceed to checkout") {
+        if ($bevestiging == "proceed") {
+
+            $db = new DbHandler("USER");
+            $connection = $db->connect();
+            //insert new ordernumber with usernumber
+            //replace with real user ID in final product
+            //$userID=$_SESSION["id"];
+            $userID=3;
+            $sql = "INSERT INTO orderrecord (customerID) VALUES (:id)";
+            $stmt = $connection->prepare($sql);
+            $stmt->execute([':id' => $userID]);
+            sleep(0.25);
+            //select orderrecordID from table to use in product insert
+            $sql = "SELECT orderrecordID FROM orderrecord WHERE customerID=:id ORDER BY date DESC LIMIT 1";
+            $stmt = $connection->prepare($sql);
+            $stmt->execute([':id' => $userID]);
+            $result = $stmt->fetch();
+            sleep(0.25);
+            $orderID=($result["orderrecordID"]);
+
+            //test data----------------------------------------------------------
+//            $productArray=($_SESSION["shoppingCart"]);
+            $productArray=array(1, 18, 25, 69);
+
+            //end test data-------------------------------------------------------
+
+            //use foreach to place each product in order
+            foreach ($productArray as $product) {
+//                $amountArray=($_SESSION["aantallen"]);
+                $amountArray= array(25=>6, 69=>69);
+
+                //check the amount and set if exists
+                if ($product == (array_key_exists($product, $amountArray))) {
+                    $amount = ($amountArray["$product"]);
+                } else {
+                    $amount = 1;
+                }
+                //done with gathering orderdata
+
+                //start of inserting into orderproduct
+                //search product name & price to use in insert
+                $pDb = new DbHandler("ERP");
+                $pConnection = $pDb->connect();
+                //replace StockItemID with StockItemName later
+                $pSql = "SELECT StockItemID, RecommendedRetailPrice FROM stockitems WHERE StockItemID=:pid";
+                $pStmt = $pConnection->prepare($pSql);
+                $pStmt->execute([':pid' => $product]);
+                $pResult = $pStmt->fetch();
+                $pDb->disconnect();
+                $pDb = null;
+                sleep(0.25);
+
+                //use insert into orderproduct for orderrecordID, productName, amount, UnitPrice
+                $pDb = new DbHandler("USER");
+                $pConnection = $pDb->connect();
+                //replace StockItemID with StockItemName later
+                $pSql = "INSERT INTO orderproduct (orderrecordID, productID, amount, unitPrice) VALUES (:oid, :pid, :amount,:price)";
+                $pStmt = $pConnection->prepare($pSql);
+                //replace StockItemID with StockItemName later
+                $pStmt->execute([':oid' => $orderID, ':pid' => ($pResult["StockItemID"]), ':amount' => $amount, ':price' => ($pResult["RecommendedRetailPrice"])]);
+                $pDb->disconnect();
+                $pDb = null;
+
+            }
+
+            //close connection
+            $db->disconnect();
+            $db = null;
+
             print('
         <div class="row">
             <div class="col-12">
--- public/orderGeschiedenis.php
+++ public/orderGeschiedenis.php
@@ -8,15 +8,16 @@ print "<link href='css/productOverzicht.css' rel='stylesheet'>";
 
 print "<div class='container'>";
 //placeholder for user ID
+//$userID=$_SESSION["id"];
 $userID=3;
 
 if (isset($userID)) {
-    print "Hier komt de order geschiedenis van de gebruiker<br>";
+//  Hier komt de order geschiedenis van de gebruiker
 
     //start db connectie
     $db = new DbHandler("USER");
     $connection = $db->connect();
-    $sql = "SELECT O.orderrecordID, O.date, OP.productID, OP.amount FROM orderrecord O JOIN orderproduct OP ON O.orderrecordID=OP.orderrecordID WHERE O.customerID=:id";
+    $sql = "SELECT O.orderrecordID, O.date, OP.productID, OP.amount, OP.unitPrice FROM orderrecord O JOIN orderproduct OP ON O.orderrecordID=OP.orderrecordID WHERE O.customerID=:id";
     $stmt = $connection->prepare($sql);
     $stmt->execute([':id' => $userID]);
     $results = $stmt->fetchAll();
@@ -25,22 +26,34 @@ if (isset($userID)) {
     $db = null;
 
 
-    $inOrder=0;
+    $inOrder=null;
     foreach($results as $result) {
         $recordID=($result["orderrecordID"]);
         $date = ($result["date"]);
-        $pid= ($result["productID"]);
-        $amount= ($result["amount"]);
 
         //if orderrecordID is the same as previous orderrecord make new div
         if ($recordID!=$inOrder) {
 //            dit komt in een nieuwe div
-            print "<br> Ordernummer: $recordID";
-            print "&nbsp&nbsp&nbsp op $date<br>";
+            print "<div class='card'>";
+            print "<div class='card-header'>Ordernummer: $recordID";
+            print "&nbsp&nbsp&nbsp datum: $date<br>";
+            $totaal=0;
+            foreach ($results as $result) {
+                $pid= ($result["productID"]);
+                $amount= ($result["amount"]);
+                $unitPrice= ($result["unitPrice"]);
+                print "<ul class='list-group list-group-flush'>";
+                if ($recordID==$result["orderrecordID"]) {
+                    print "<li class='list-group-item'>Product: ".$pid." &nbsp&nbsp&nbsp aantal: ".$amount." &nbsp&nbsp&nbsp prijs per product: €$unitPrice</li><br>";
+                    $totaal= $totaal+($amount*$unitPrice);
+                }
+                print "</ul>";
+            }
+            print "Totaalprijs: €$totaal</div>";
+            print "</div><br>";
         }
 
         //insert items into orderrecordID div
-        print "Product: ".$pid." ".$amount." keer<br>";
 
         //make clear for next loop that previous orderrecord==$inOrder
         $inOrder=$recordID;
@@ -50,7 +63,8 @@ if (isset($userID)) {
     print "Er zijn geen eerdere aankopen gevonden :(";
 }
 
-
-
+?>
+</div>
+<?php
 include_once("../public/includes/footer.php");
 ?>
