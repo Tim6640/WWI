b9aee56ea821f7deec491f3a012ab4ee1a1bd712
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-12-16 13:30:21 +0100

    added verlanglijstje.php

--- public/verlanglijstje.php
+++ public/verlanglijstje.php
@@ -4,11 +4,171 @@ include_once("../src/core/init.php");
 $pageTitle = "verlanglijstje";
 include_once("../public/includes/header.php");
 
-$db = new DbHandler("ERP");
-$connection = $db->connect();
-$sql = "SELECT StockItemID, StockItemName, RecommendedRetailPrice, Photo FROM stockitems WHERE StockItemID IN (Select StockitemID from Wishlist where customerID = $thisCustomerId";
-$stmt = $connection->prepare($sql);
-$stmt->execute();
-$products = $stmt->fetchAll();
-$db->disconnect();
-$db = null;
+?>
+    <div class='container'></div>
+    <link href='css/productOverzicht.css' rel='stylesheet'>
+    <script
+        src='https://code.jquery.com/jquery-3.4.1.min.js'
+        integrity='sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo='
+        crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
+            integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1'
+            crossorigin='anonymous'></script>
+    <script>
+        function startAjax($productNummer) {
+            var wagen = "wagen=";
+            $.ajax({
+                    type: "POST",
+                    url: "productOverzicht.php",
+                    data: wagen + $productNummer,
+                    success: function (result) {
+                        console.log("Yes!");
+                        console.log($productNummer)
+                    }
+                }
+            );
+        }
+    </script>
+    <div class='container'>
+        <div class='modal' tabindex='-1' id='banaan' role='dialog'>
+            <div class='modal-dialog modal-dialog-centered' role='document'>
+                <div class='modal-content'>
+                    <div class='modal-header'>
+                        <h5 class='modal-title'>Wilt u verder winkelen of doorgaan naar de winkelwagen</h5>
+                        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                            <span aria-hidden='true'>&times;</span>
+                        </button>
+                    </div>
+                    <div class='modal-body'>
+                        <a type='button' class='btn btn-primary btn-lg bnt-block my-3' href='winkelmandje.php'>
+                            Doorgaan naar de winkelwagen
+                        </a>
+                        <a type='button' class='btn btn-primary btn-lg bnt-block' href=''>
+                            Verder winkelen
+                        </a>
+                    </div>
+                    <div class='modal-footer'>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<?php
+//placeholder for user ID
+
+$_SESSION["id"]=3;
+
+//put in delete query
+
+
+if (isset($_SESSION["id"])) {
+    $userID=$_SESSION["id"];
+    $db = new DbHandler("USER");
+    $connection = $db->connect();
+    $sql = "SELECT productId FROM wishlist WHERE customerID=:id";
+    $stmt = $connection->prepare($sql);
+    $stmt->execute([':id' => $userID]);
+    $products = $stmt->fetchAll();
+    $db->disconnect();
+    $db = null;
+
+    if (empty($products)) {
+        print "U heeft geen producten in uw verlanglijstje.<img src='sad.gif'>";
+    } else {
+//        print_r($products);
+//            $pids=($products["0"]);
+//            print "<br><br>".$pids;
+//            $products = implode(', ', ($products["productId"]));
+
+
+        foreach ($products as $product) {
+            $product=($product["productId"]);
+            $db = new DbHandler("ERP");
+            $connection = $db->connect();
+            $sql = "SELECT StockItemID, StockItemName, RecommendedRetailPrice, Photo FROM stockitems WHERE StockItemID=:id";
+            $stmt = $connection->prepare($sql);
+            $stmt->execute([':id' => $product]);
+            $result = $stmt->fetch();
+            $db->disconnect();
+            $db = null;
+
+            $pid=$result["StockItemID"];
+            $name=$result["StockItemName"];
+            $price=$result["RecommendedRetailPrice"];
+
+            print "<div class='card no_border'>
+            <div class='card-body'>
+
+            <!--print product image-->
+            <div class='d-flex'>
+            <a href='product.php?pid=" . $pid . "/'<br>
+            <div class='p-2'>
+            <img src='images\productPlaceholder.png' class='product_image'>
+            </a>
+            </div>
+
+            <!--print title, review and price-->
+            <a href='product.php?pid=" . $pid . "'<br>
+            <div class='ml-auto p-2 a_text'>
+            <b class='card-title'>" . $name . "</b>";
+
+            //look up review----------------------------------------------------------------------------------
+            $db = new DbHandler("USER");
+            $connection = $db->connect();
+            $sql = "SELECT AVG(score) as score FROM review WHERE productID=:pid";
+            $stmt = $connection->prepare($sql);
+            $stmt->execute([':pid'=>$pid]);
+            $result = $stmt->fetch();
+            if ($result["score"]==0) {
+                print "<h6>Score: geen rating</h6>";
+
+            } else {
+                print "<h6> Score: ";
+                for ($i=0;$i<(round($result["score"], 0));$i++) {
+                    print "<span class='fa fa-star' style='color:gold'></span>";
+                }
+                print "</h6>";
+            }
+            $db->disconnect();
+            $db = null;
+
+            //look up and print price
+            print "<h6>€ " . $price . "</h6>
+            </div>
+            </a>
+
+            <!--print icons-->
+            <div class='ml-auto my-auto'>";
+
+            //send session page to shopping cart
+// link naar winkelmand en verlanglijstje
+            print "<form>
+                <button formmethod='post' name='wagen' type='button' value='" . $pid . "' class='btn' data-toggle='modal' data-target='#banaan' onclick='startAjax($pid)'>
+                    <i style='color:#00BDF3' class='color: fas fa-cart-plus fa-2x'></i>
+                </button>
+            </form>
+            <form>
+                <button formmethod='post' name='remove' type='button' value='" . $pid . "' class='btn' data-toggle='modal' data-target='#banaan' onclick=''>
+                    <i style='color:#00BDF3' class='fas fa-heart-broken fa-2x'></i>
+                </button>
+            </form>
+            <!--add function to add product to cart-->
+            </div>
+        </div>
+            
+            </div>
+            </div>";
+
+
+        }
+    }
+
+
+
+} else {
+    print "sorry, u moet ingelogd zijn om deze pagina te bekijken";
+}
+
+
+?>
