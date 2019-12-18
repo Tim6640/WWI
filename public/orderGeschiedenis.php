ba1126c664bfd9994af59e60b0b623907469d1b0
BasbrandiusMaximus <bastian.schiphouwer@gmail.com>
2019-12-18 14:47:50 +0100

    fixed the purchase process

--- public/orderGeschiedenis.php
+++ public/orderGeschiedenis.php
@@ -14,11 +14,10 @@ $userID=$_SESSION["id"];
 
 if (isset($userID)) {
 //  Hier komt de order geschiedenis van de gebruiker
-
     //start db connectie
     $db = new DbHandler("USER");
     $connection = $db->connect();
-    $sql = "SELECT O.orderrecordID, O.date, OP.productID, OP.amount, OP.unitPrice FROM orderrecord O JOIN orderproduct OP ON O.orderrecordID=OP.orderrecordID WHERE O.customerID=:id";
+    $sql = "SELECT O.orderrecordID, O.date, OP.productName, OP.amount, OP.unitPrice FROM orderrecord O JOIN orderproduct OP ON O.orderrecordID=OP.orderrecordID WHERE O.customerID=:id";
     $stmt = $connection->prepare($sql);
     $stmt->execute([':id' => $userID]);
     $results = $stmt->fetchAll();
@@ -26,41 +25,47 @@ if (isset($userID)) {
     $db->disconnect();
     $db = null;
 
+    if (!empty($results)) {
+        print "<h1 class='text-lg-center'>Order Geschiedenis</h1>";
+        $inOrder=null;
+        foreach($results as $result) {
+            $recordID=($result["orderrecordID"]);
+            $date = date("d-m-Y", strtotime($result["date"]));
 
-    $inOrder=null;
-    foreach($results as $result) {
-        $recordID=($result["orderrecordID"]);
-        $date = date("d-m-Y", strtotime($result["date"]));
-
-        //if orderrecordID is the same as previous orderrecord make new div
-        if ($recordID!=$inOrder) {
+            //if orderrecordID is the same as previous orderrecord make new div
+            if ($recordID!=$inOrder) {
 //            dit komt in een nieuwe div
-            print "<div class='card'>".
-            "<div class='card-header'>Ordernummer: $recordID&nbsp&nbsp&nbsp&nbsp".
-            "<span class='mr-auto'>datum: $date</span></div>";
-            $totaal=0;
-            foreach ($results as $result) {
-                $pid= ($result["productID"]);
-                $amount= ($result["amount"]);
-                $unitPrice= ($result["unitPrice"]);
-                print "<div><ul class='list-group list-group-flush'>";
-                if ($recordID==$result["orderrecordID"]) {
-                    print "<li class='m-auto my-auto list-group-item'>".
-                    "Product: ".$pid."&nbsp&nbsp&nbsp".
-                    "aantal: ".$amount." &nbsp&nbsp&nbsp prijs: €$unitPrice</li><br>";
-                    $totaal= $totaal+($amount*$unitPrice);
+                print "<div class='card'>".
+                    "<div class='card-header' style='color:white; background-color:blue'>Ordernummer: $recordID&nbsp&nbsp&nbsp&nbsp".
+                    "<span class='mr-auto'>datum: $date</span></div>";
+                $totaal=0;
+                foreach ($results as $result) {
+                    $pid= ($result["productName"]);
+                    $amount= ($result["amount"]);
+                    $unitPrice= ($result["unitPrice"]);
+                    print "<div><ul class='list-group list-group-flush'>";
+                    if ($recordID==$result["orderrecordID"]) {
+                        print "<li class='m-auto my-auto list-group-item'>".
+                            "Product: ".$pid."&nbsp&nbsp&nbsp".
+                            "aantal: ".$amount." &nbsp&nbsp&nbsp prijs: €$unitPrice</li><br>";
+                        $totaal= $totaal+($amount*$unitPrice);
+                    }
+                    print "</ul></div>";
                 }
-                print "</ul></div>";
+                print "<div class='m-auto'>Totaalprijs: €$totaal</div></div><br>";
             }
-            print "<div class='m-auto'>Totaalprijs: €$totaal</div></div><br>";
+            //make clear for next loop that previous orderrecord==$inOrder
+            $inOrder=$recordID;
         }
-        //make clear for next loop that previous orderrecord==$inOrder
-        $inOrder=$recordID;
+
+    } else {
+        print "Er zijn geen eerdere aankopen gevonden";
     }
+
 } else {
     //wanneer er geen geschiedenis is van de bestelling
     ?>
-    <div zijn geen eerdere aankopen gevonden</div>
+    <div U moet ingelogd zijn om deze pagina te bekijken</div>
     <?php
 }
 
--- public/winkelmandje.php
+++ public/winkelmandje.php
@@ -196,7 +196,7 @@ if(!empty($_SESSION["shoppingCart"])) {
 
                         </ul>
                         <form method="post" action="betalingbevestiging.php">
-                        <input type="submit" name="submit" value="proceed to checkout"
+                        <input type="submit" name="bevestiging" value="proceed"
                                class="btn btn-primary rounded-pill py-2 btn-block">
                         </form>
                     </div>
@@ -225,5 +225,4 @@ else{
     </div>
 </div>
     <?php }
-
     include_once("includes/footer.php")?>
